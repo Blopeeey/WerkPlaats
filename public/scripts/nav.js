3236890cb87a8318a5ef6fb6eb175e5fa1ec3ec6
Zt-freak <bnkho98@hotmail.com>
2019-11-16 21:10:46 +0100

    made class names BEM compliant

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -12,34 +12,34 @@ window.addEventListener('scroll', function (event) {
     event.preventDefault();
 
     if (window.scrollY >= 50) {
-        nav.classList.add("navigation-scrolled");
-        nav.classList.remove("navigation-nonscrolled");
+        nav.classList.add("navigation--scrolled");
+        nav.classList.remove("navigation--nonscrolled");
 
-        navMenu.classList.add("navmenu-scrolled");
-        navMenu.classList.remove("navmenu-nonscrolled");
+        navMenu.classList.add("navmenu--scrolled");
+        navMenu.classList.remove("navmenu--nonscrolled");
 
-        hamburger.classList.add("hamburger-scrolled");
-        hamburger.classList.remove("hamburger-nonscrolled");
+        hamburger.classList.add("hamburger--scrolled");
+        hamburger.classList.remove("hamburger--nonscrolled");
 
         navlogo.src = "images/logo-blacktext.png";
         for (i = 0; i < navOptions.length; ++i) {
-            navOptions[i].classList.add("navoption-scrolled");
-            navOptions[i].classList.remove("navoption-nonscrolled");
+            navOptions[i].classList.add("navoption--scrolled");
+            navOptions[i].classList.remove("navoption--nonscrolled");
         }
     } else {
-        nav.classList.add("navigation-nonscrolled");
-        nav.classList.remove("navigation-scrolled");
+        nav.classList.add("navigation--nonscrolled");
+        nav.classList.remove("navigation--scrolled");
 
-        navMenu.classList.add("navmenu-nonscrolled");
-        navMenu.classList.remove("navmenu-scrolled");
+        navMenu.classList.add("navmenu--nonscrolled");
+        navMenu.classList.remove("navmenu--scrolled");
 
-        hamburger.classList.add("hamburger-nonscrolled");
-        hamburger.classList.remove("hamburger-scrolled");
+        hamburger.classList.add("hamburger--nonscrolled");
+        hamburger.classList.remove("hamburger--scrolled");
 
         navlogo.src = "images/logo.png";
         for (i = 0; i < navOptions.length; ++i) {
-            navOptions[i].classList.add("navoption-nonscrolled");
-            navOptions[i].classList.remove("navoption-scrolled");
+            navOptions[i].classList.add("navoption--nonscrolled");
+            navOptions[i].classList.remove("navoption--scrolled");
         }
     }
 });
@@ -50,13 +50,13 @@ window.addEventListener('scroll', function (event) {
 hamburger.addEventListener('click', function (event) {
     event.preventDefault();
     if (hamburgerClicked === false) {
-        navMenu.classList.add("navmenu-visible");
-        navMenu.classList.remove("navmenu-invisible");
+        navMenu.classList.add("navmenu--visible");
+        navMenu.classList.remove("navmenu--invisible");
         hamburgerClicked = true;
     }
     else {
-        navMenu.classList.add("navmenu-invisible");
-        navMenu.classList.remove("navmenu-visible");
+        navMenu.classList.add("navmenu--invisible");
+        navMenu.classList.remove("navmenu--visible");
         hamburgerClicked = false;
     }
 });
@@ -67,12 +67,12 @@ hamburger.addEventListener('click', function (event) {
 window.addEventListener('resize', function (event) {
     event.preventDefault();
     if (window.innerWidth >= 800) {
-        navMenu.classList.add("navmenu-visible");
-        navMenu.classList.remove("navmenu-invisible");
+        navMenu.classList.add("navmenu--visible");
+        navMenu.classList.remove("navmenu--invisible");
     }
     else {
-        navMenu.classList.add("navmenu-invisible");
-        navMenu.classList.remove("navmenu-visible");
+        navMenu.classList.add("navmenu--invisible");
+        navMenu.classList.remove("navmenu--visible");
         hamburgerClicked = false;
     }
 });
