1de06046a17c6238fa432ae2621a29dbfcab1c9d
Zt-freak <bnkho98@hotmail.com>
2019-11-16 18:54:24 +0100

    refactor

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,51 +1,78 @@
-const nav = document.querySelector('nav');
-const navLogo = document.getElementById('navlogo');
-const navOptions = document.querySelectorAll('#navoption');
-const hamburger = document.getElementById('hamburger');
-let hamburgerClicked = false;
-const navMenu = document.getElementById('navMenu');
+const nav = document.querySelector('nav'); // whole nav element
+const navLogo = document.getElementById('navlogo'); // navigation logo
+const navOptions = document.querySelectorAll('#navoption'); // array of navigation options
+const hamburger = document.getElementById('hamburger'); // hamburger menu icon
+let hamburgerClicked = false; // whether the hamburger menu is active or not
+const navMenu = document.getElementById('navMenu'); // container for the navigation options
 
+/*
+* change navigation colour on scroll
+*/
 window.addEventListener('scroll', function (event) {
     event.preventDefault();
 
     if (window.scrollY >= 50) {
-        nav.style.backgroundColor = '#FFFFFF';
-        navMenu.style.backgroundColor = '#FFFFFF';
-        hamburger.style.backgroundImage = "url(\"images/hamburger-black.svg\")";
+        nav.classList.add("nav-scrolled");
+        nav.classList.remove("nav-nonscrolled");
+
+        navMenu.classList.add("navmenu-scrolled");
+        navMenu.classList.remove("navmenu-nonscrolled");
+
+        hamburger.classList.add("hamburger-scrolled");
+        hamburger.classList.remove("hamburger-nonscrolled");
+
         navlogo.src = "images/logo-blacktext.png";
         for (i = 0; i < navOptions.length; ++i) {
-            navOptions[i].style.color = "#000000";
+            navOptions[i].classList.add("navoption-scrolled");
+            navOptions[i].classList.remove("navoption-nonscrolled");
         }
     } else {
-        nav.style.backgroundColor = 'transparent';
-        navMenu.style.backgroundColor = 'transparent';
-        hamburger.style.backgroundImage = "url(\"images/hamburger-white.svg\")";
+        nav.classList.add("nav-nonscrolled");
+        nav.classList.remove("nav-scrolled");
+
+        navMenu.classList.add("navmenu-nonscrolled");
+        navMenu.classList.remove("navmenu-scrolled");
+
+        hamburger.classList.add("hamburger-nonscrolled");
+        hamburger.classList.remove("hamburger-scrolled");
+
         navlogo.src = "images/logo.png";
         for (i = 0; i < navOptions.length; ++i) {
-            navOptions[i].style.color = "#FFFFFF";
+            navOptions[i].classList.add("navoption-nonscrolled");
+            navOptions[i].classList.remove("navoption-scrolled");
         }
     }
 });
 
+/*
+* activate/deactivate hamburger menu on click
+*/
 hamburger.addEventListener('click', function (event) {
     event.preventDefault();
     if (hamburgerClicked === false) {
-        navMenu.style.display = "flex";
+        navMenu.classList.add("navmenu-visible");
+        navMenu.classList.remove("navmenu-invisible");
         hamburgerClicked = true;
     }
     else {
-        navMenu.style.display = "none";
+        navMenu.classList.add("navmenu-invisible");
+        navMenu.classList.remove("navmenu-visible");
         hamburgerClicked = false;
     }
 });
 
+/*
+* activate/deactivate hamburger menu on resize
+*/
 window.addEventListener('resize', function (event) {
     event.preventDefault();
     if (window.innerWidth >= 800) {
-        navMenu.style.display = "flex";
+        navMenu.classList.add("navmenu-visible");
+        navMenu.classList.remove("navmenu-invisible");
     }
     else {
-        navMenu.style.display = "none";
+        navMenu.classList.add("navmenu-invisible");
+        navMenu.classList.remove("navmenu-visible");
         hamburgerClicked = false;
     }
 });
